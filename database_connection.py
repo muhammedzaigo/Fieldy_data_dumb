19689d86a0a7ba6a72400e900f67fe1b73779d33
zaigobala <bala@zaigoinfotech.com>
2023-02-28 16:49:00 +0530

    hash password and update db connection

--- database_connection.py
+++ database_connection.py
@@ -6,53 +6,51 @@ PORT = 3306
 PASSWORD = '7592978136'
 DATABASE = 'prod_live'
 
-connection = pymysql.connect(host=HOST,user=USER,port=PORT,password=PASSWORD,db=DATABASE)
-
 def insert_update_delete(qry,val):
-    con=connection
-    cmd=con.cursor()
+    connection=pymysql.connect(host=HOST,user=USER,port=PORT,password=PASSWORD,db=DATABASE)
+    cmd=connection.cursor()
     cmd.execute(qry,val)
     id=cmd.lastrowid
-    con.commit()
-    con.close()
+    connection.commit()
+    connection.close()
     return id
 
 
 def insert_update_delete_many(qry,val):
-    con=connection
-    cmd=con.cursor()
+    connection=pymysql.connect(host=HOST,user=USER,port=PORT,password=PASSWORD,db=DATABASE)
+    cmd=connection.cursor()
     cmd.executemany(qry,val)
     rows=cmd.lastrowid    
-    con.commit()
-    con.close()
+    connection.commit()
+    connection.close()
     return rows
 
 
 def select_all(qry):
-    con=connection
-    cmd=con.cursor()
+    connection=pymysql.connect(host=HOST,user=USER,port=PORT,password=PASSWORD,db=DATABASE)
+    cmd=connection.cursor()
     cmd.execute(qry)
     res=cmd.fetchall()
-    con.commit()
-    con.close()
+    connection.commit()
+    connection.close()
     return res
 
 
 def select_filter(qry,val):
-    con=connection
-    cmd=con.cursor()
+    connection=pymysql.connect(host=HOST,user=USER,port=PORT,password=PASSWORD,db=DATABASE)
+    cmd=connection.cursor()
     cmd.execute(qry,val)
     res=cmd.fetchall()
-    con.commit()
-    con.close()
+    connection.commit()
+    connection.close()
     return res
 
 
 def select_one(qry,val):
-    con=connection
-    cmd=con.cursor()
+    connection=pymysql.connect(host=HOST,user=USER,port=PORT,password=PASSWORD,db=DATABASE)
+    cmd=connection.cursor()
     cmd.execute(qry,val)
     res=cmd.fetchone()
-    con.commit()
-    con.close()
+    connection.commit()
+    connection.close()
     return res
